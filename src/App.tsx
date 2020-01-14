371fbed225fc7a37c91060b19d1816aac589b941
Jang Haemin <io@jhaemin.com>
2020-01-14 15:43:13 -0500

    Init the project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.scss'
+
+const App: React.FC = () => {
+  return (
+    <div id="app">
+      <h1>Clowd Desktop</h1>
+    </div>
+  )
+}
+
+export default App
