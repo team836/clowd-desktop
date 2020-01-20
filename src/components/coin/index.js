0f37707d7f7e0abd7af74f9b807efd7667f4b3d5
chaehoonki <chaeefe@gmail.com>
2020-01-20 13:01:06 -0500

    feat: add coin image

--- src/components/coin/index.js
+++ src/components/coin/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './style.css';
+import Icon from '../../assets/images/coin.jpg';
+
 
 const { ipcRenderer } = window.require('electron')
 
 ipcRenderer.on('asynchronous-reply', (event, arg) => {
     console.log(arg) // "pong"이 출력됩니다.
 })
-
 
 const Coin = () => {
 
@@ -15,11 +16,10 @@ const Coin = () => {
         ipcRenderer.send('asynchronous-message', 'ping')
     }
 
-
     return (
         <div className="coin">
-            coin
-        <button className="dialog-open" onClick={ping}>open</button>
+            <img className="coin-image" src={Icon} />
+            <button className="dialog-open" onClick={ping}>open</button>
         </div>
     );
 }
