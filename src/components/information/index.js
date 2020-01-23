bf66ed08c70d3e68030ad7074e8f3cd6e8599049
chaehoonki <chaeefe@gmail.com>
2020-01-23 09:57:21 -0500

    chore: fix indentation

--- src/components/information/index.js
+++ src/components/information/index.js
@@ -1,31 +1,31 @@
-import React from 'react';
-import './style.css';
+import React from 'react'
+import './style.css'
 const Information = () => {
-    return (
-        <div className="information">
-            <div className="info">
-                <label className="info-subject">Ping</label>
-                <br />
-                <label className="info-number">82</label>
-                <label className="info-unit">ms</label>
-                <br />
-            </div>
-            <div className="info">
-                <label className="info-subject">Upload</label>
-                <br />
-                <label className="info-number">15</label>
-                <label className="info-unit">Mbps</label>
-                <br />
-            </div>
-            <div className="info">
-                <label className="info-subject">Download</label>
-                <br />
-                <label className="info-number">17</label>
-                <label className="info-unit">Mbps</label>
-                <br />
-            </div>
-        </div>
-    );
+  return (
+    <div className="information">
+      <div className="info">
+        <label className="info-subject">Ping</label>
+        <br />
+        <label className="info-number">82</label>
+        <label className="info-unit">ms</label>
+        <br />
+      </div>
+      <div className="info">
+        <label className="info-subject">Upload</label>
+        <br />
+        <label className="info-number">15</label>
+        <label className="info-unit">Mbps</label>
+        <br />
+      </div>
+      <div className="info">
+        <label className="info-subject">Download</label>
+        <br />
+        <label className="info-number">17</label>
+        <label className="info-unit">Mbps</label>
+        <br />
+      </div>
+    </div>
+  )
 }
 
-export default Information;
+export default Information
