236e1d0d518b2b263be6dd077b7da1907c96dcce
chaehoonki <chaeefe@gmail.com>
2020-01-23 20:46:27 -0500

    refactor: refactoring

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,21 +1,20 @@
-import React from 'react';
-import './style.css';
-import Information from 'components/information';
-import Chart from 'components/chart';
+import React from 'react'
+import './style.css'
+import Information from 'components/information'
+import DataChart from 'components/chart'
 
 const DashBoard = () => {
-
-    return (
-        <div className="dashboard">
-            <div className="header">
-                <div className="dashboard-title"> Dashborad </div>
-                <div className="setting"></div>
-            </div>
-            <div className="main">
-                <Chart />
-                <Information />
-            </div>
-        </div>
-    );
+  return (
+    <div className="dashboard">
+      <div className="header">
+        <div className="dashboard-title"> Dashborad </div>
+        <div className="setting"></div>
+      </div>
+      <div className="main">
+        <DataChart />
+        <Information />
+      </div>
+    </div>
+  )
 }
-export default DashBoard;
+export default DashBoard
