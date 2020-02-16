3ec4b02d82f4c3644857218f169e96f5c00b1284
chaehoonki <chaeefe@gmail.com>
2020-02-15 20:06:57 -0500

    refactor: fit data unit KB->GB

--- src/main-process/systemVariable.js
+++ src/main-process/systemVariable.js
@@ -4,21 +4,25 @@ class SystemVariable {
       return SystemVariable.instance
     }
     SystemVariable.instance = this
-    this.diskSize = 0 // disk total size KB
-    this.diskFree = 0 // disk remain size KB
-    this.folderUsage = 0 // folder using size KB
-    this.settingSize = 1000000 // set user total KB
-    this.capacity = 0 // min(free ,total-usage) KB
+    this.diskSize = 0 // disk total size GB
+    this.diskFree = 0 // disk remain size GB
+    this.folderUsage = 0 // folder using size GB
+    this.settingSize = 50 // set user total GB
+    this.minSettingSize = 0
+    this.maxSettingSize = 100
+    this.capacity = 0 // min(free ,total-usage) GB
     this.bandwidth = 0 //fetch from server Mbps
     return this
   }
   print() {
+    console.log('=====================')
     console.log(`diskSize ${this.diskSize}`)
     console.log(`diskFree ${this.diskFree}`)
     console.log(`folderUsage ${this.folderUsage}`)
     console.log(`settingSize ${this.settingSize}`)
     console.log(`capacity ${this.capacity}`)
     console.log(`bandwidth ${this.bandwidth}`)
+    console.log('=====================')
   }
 }
 
