756da7a1463218e187ab3a99258b9d75339d99bb
chaehoonki <chaeefe@gmail.com>
2020-02-19 13:24:33 -0500

    feat: reset

--- src/main-process/pathfile.js
+++
@@ -1,4 +0,0 @@
-const SERVER = 'wss://dev.clowd.xyz/v1/node'
-const LOCALDIR = 'C:\\Users\\chea1\\AppData\\Local\\clowd\\'
-
-module.exports = { SERVER, LOCALDIR }
--- src/screens/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as Login } from './login'
-export { default as Main } from './main'
