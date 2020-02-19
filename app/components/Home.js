d637514f005adcd51c94c70447c4abdac72c8ddc
chaehoonki <chaeefe@gmail.com>
2020-02-19 14:57:35 -0500

    feat: login page

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,20 +1,36 @@
 // @flow
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import routes from '../constants/routes.json';
+import React, { useRef } from 'react';
+// import { Link } from 'react-router-dom';
+// import routes from '../constants/routes.json';
 import styles from './Home.css';
+import SignIn from '../../resources/google-signIn.jpg';
 
-type Props = {};
+const { ipcRenderer } = window.require('electron');
 
-export default class Home extends Component<Props> {
-  props: Props;
+export default function Home() {
+  const buttonFocus = useRef();
 
-  render() {
-    return (
-      <div className={styles.container} data-tid="container">
-        <h2>Home</h2>
-        <Link to={routes.COUNTER}>to Counter</Link>
+  const handleClick = () => {
+    ipcRenderer.send('google-signIn', 'sign-in');
+  };
+  const handleKeyDown = ev => {
+    if (ev.keyCode === 13) {
+      handleClick();
+    }
+  };
+  return (
+    <div className={styles.loginpage}>
+      <div
+        className={styles.imageWrapper}
+        ref={buttonFocus}
+        onClick={handleClick}
+        onKeyDown={handleKeyDown}
+        role="button"
+        tabIndex={0}
+      >
+        <img src={SignIn} className="sign-in" alt="sign-in" />
       </div>
-    );
-  }
+      {/* <Link to={routes.COUNTER}>to Counter</Link> */}
+    </div>
+  );
 }
