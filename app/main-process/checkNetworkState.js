3e5acfeef06d4e4ad1c08d4b580ce0e371a49a71
chaehoonki <chaeefe@gmail.com>
2020-02-22 15:14:19 -0500

    chore: delete unused codes

--- app/main-process/checkNetworkState.js
+++
@@ -1,9 +0,0 @@
-// import { exec } from 'child_process';
-// import speedTest from 'speedtest-net';
-
-// async function checkNetwork() {
-//   const speed = await speedTest();
-//   return speed.download.bandwidth;
-// }
-
-// export default checkNetwork;
