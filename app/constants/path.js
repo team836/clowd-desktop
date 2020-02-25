c6e2ae88111a1a91cc51ca6ccd1d6e5d102b25fa
chaehoonki <chaeefe@gmail.com>
2020-02-25 00:25:03 -0500

    chore: version setting path change

--- app/constants/path.js
+++ app/constants/path.js
@@ -1,7 +1,7 @@
 import os from 'os';
 import path from 'path';
 
-const SOCKETSERVER = 'wss://dev.clowd.xyz/v1/node';
+const SOCKETSERVER = 'wss://clowd.xyz/v1/node';
 const FOLDERPATH =
   process.platform === 'win32'
     ? path.join(os.userInfo().homedir, 'AppData/Local/clowd/')
